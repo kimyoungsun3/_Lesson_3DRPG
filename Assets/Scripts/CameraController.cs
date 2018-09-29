2e743b44279c888db8a876b762e23c1387e377c6
kimyoungsun <kyssmart@naver.com>
2018-09-29 14:54:48 +0900

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform target;
+	public Vector3 offset;
+	public float pitch = 2f;
+	public float zoomSpeed = 4f;
+	public float zoomMin = 5f;
+	public float zoomMax = 15;
+	public float yawSpeed = 100f;
+	private float currentZoom = 10f;
+	private float currentYaw = 0f;
+
+	void LateUpdate () {
+		currentZoom -= Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
+		currentZoom = Mathf.Clamp (currentZoom, zoomMin, zoomMax);
+		currentYaw -= Input.GetAxis ("Horizontal") * yawSpeed * Time.deltaTime;
+
+		transform.position = target.position - offset * currentZoom;
+		transform.LookAt (target.position + Vector3.up * pitch);
+		transform.RotateAround(target.position, Vector3.up, currentYaw);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+	public float distance = 100f;
+	public LayerMask groundMask;
+
+	PlayerMotor motor;
+	Camera cam;
+
+	void Start () {
+		cam = Camera.main;	
+		motor = GetComponent<PlayerMotor> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//mouse click and character move.
+		if (Input.GetMouseButtonDown (0)) {
+			Ray _ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit _hit;
+
+			if(Physics.Raycast(_ray, out _hit, distance, groundMask)){
+				Debug.Log("hit point " + _hit.collider.name + " " + _hit.point);
+				motor.MoveToPoint (_hit.point);
+			}
+		}
+
+		//mouse right click and item eat
+		if (Input.GetMouseButtonDown (1)) {
+			Ray _ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit _hit;
+
+			if(Physics.Raycast(_ray, out _hit, distance)){
+				//Check if we hit an interactable.
+				//if we did set it as our forus.
+			}
+		}
+	}
+}
